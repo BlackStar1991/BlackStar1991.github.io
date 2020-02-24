1e716a25cd4cc693e5055f321ec96988bdf990ae
Zirka Andry <a.zirka@cleack.com>
2020-02-24 16:54:34 +0200

    corrected styles

--- js/onlyFor_aboutCompany-page.js
+++ js/onlyFor_aboutCompany-page.js
@@ -2,11 +2,14 @@ $(document).ready(function () {
 
 
     var body = $("body"),
-        WrapperHeight = $(".top_element__whoWeAre").height(),
         lantern1 = $(".whoWeAre_lantern__1"),
         lantern2 = $(".whoWeAre_lantern__2"),
-        lantern3 = $(".whoWeAre_lantern__3");
+        lantern3 = $(".whoWeAre_lantern__3"),
+        lantern4 = $(".whoWeAre_lantern__4"),
 
+
+        lantern5 = $(".whoWeAre_lantern__5"),
+        lantern6 = $(".whoWeAre_lantern__6");
 
 
     var doc = {
@@ -30,45 +33,68 @@ $(document).ready(function () {
         mouse.x = ((e.pageX - doc.w + doc.wC) / (doc.wC / 10));
         mouse.y = ((e.pageY - doc.h + doc.hC) / (doc.hC / 10));
 
-        lantern1.css('transform', 'translate(' + (mouse.x * 1.75) + 'px, ' + (mouse.y * 1.75 ) + 'px)');
-        lantern2.css('transform', 'translate(' + (mouse.x * 1.55) + 'px, ' + (mouse.y * 1.55) + 'px) scale(1.3)');
-        lantern3.css('transform', 'translate(' + (mouse.x * 1.3) + 'px, ' + (mouse.y * 1.3) + 'px) scale(.3)');
+        lantern1.css('transform', 'translate(' + (mouse.x * 3) + 'px, ' + (mouse.y * 3 ) + 'px) scale(.35)');
+        lantern2.css('transform', 'translate(' + (mouse.x * 10) + 'px, ' + (mouse.y * 5) + 'px) scale(1.3)');
+        lantern3.css('transform', 'translate(' + (mouse.x * 2) + 'px, ' + (mouse.y * 2.4) + 'px) scale(.4)');
+        lantern4.css('transform', 'translate(' + (mouse.x * 3) + 'px, ' + (mouse.y * 4) + 'px) scale(.35)');
+
+
+        lantern5.css('transform', 'translate(' + (mouse.x * 10) + 'px, ' + (mouse.y * 5) + 'px) scale(1.3)');
+        lantern6.css('transform', 'translate(' + (mouse.x * 3) + 'px, ' + (mouse.y * 4) + 'px) scale(.5)');
+
+
     });
 
 
+    /// WOW Animate
 
-    // $(window).scroll(function () {
-    //
-    //     var wScroll = $(this).scrollTop();
-    //
-    //     if (body.width() > 1400) {
-    //
-    //         function paralaxForLanters(correctedVal, elem, scaleLanter) {
-    //
-    //             var correctedScroll = (correctedVal) * wScroll;
-    //
-    //             if(scaleLanter === undefined){
-    //                 scaleLanter = 1;
-    //             }
-    //
-    //             if (wScroll <= WrapperHeight) {
-    //
-    //                 elem.css({
-    //                     'transform': 'translate(' + correctedScroll + '%) scale(' + scaleLanter + ')'
-    //                 });
-    //
-    //             }
-    //         }
-    //
-    //         paralaxForLanters((-0.2), lantern1);
-    //         paralaxForLanters((-0.32), lantern2, 1.3);
-    //         paralaxForLanters((-0.3), lantern3, 0.3);
-    //
-    //     }
-    //
-    // });
+    var animateLeftChildren = $(".animate-left").children(),
+        al = "animate-fadeInLeft",
+        animateRightChildren = $(".animate-right").children(),
+        ar = "animate-fadeInRight";
 
 
+    if (body.width() > 1400) {
+
+        var sectionToStart = $(".whoWeAre_mission"),
+            sectionToTop = Math.ceil( sectionToStart.offset().top ) ,
+            flagAnimate = true;
+
+        // console.log("sectionToTop = " + sectionToTop);
+
+        $(window).scroll(function () {
+
+            var currentScroll = $(this).scrollTop();
+
+
+            if ((currentScroll >= sectionToTop)) {
+                if (flagAnimate) {
+                    addAnimate(animateLeftChildren, al);
+                    addAnimate(animateRightChildren, ar);
+                    flagAnimate=false;
+                }
+            }
+
+           // console.log(currentScroll);
+
+
+        });
+
+        function addAnimate (elements, animateClass){
+           var  animClasses = animateClass;
+
+            elements.each(function (i,el, animClasses) {
+                setTimeout(function () {
+                    $(el).addClass(animateClass);
+                }, 50 + ( i * 300 ));
+
+            });
+        }
+
+
+
+
+    }
 
 
 
